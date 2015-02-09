e3a24c2680248710400e7d0950c2e04a1c2df476
calvinjwala <cjhunjhnuwala@gmail.com>
2015-02-09 15:15:59 -0800

    rerouting in app.js

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -20,7 +20,7 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html.erb',
+        templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
       .when('/about', {
