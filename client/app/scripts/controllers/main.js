d8915f7116d9dea106f3147c4b988ed02b0b8874
catlag <sammieeagle@gmail.com>
2015-02-12 21:49:18 -0800

    added modal

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -19,7 +19,7 @@
 
 
 angular.module('bookloopApp')
-  .controller('MainCtrl', ['$scope', 'Books', '$http', 'lodash',  function ($scope, Books, $http, lodash) {
+  .controller('MainCtrl', ['$scope', 'Books', '$http', 'lodash', 'transformRequestAsFormPost', function ($scope, Books, $http, lodash, transformRequestAsFormPost) {
 
   	$scope.awesomeThings = [
       'HTML5 Boilerplate',
@@ -27,31 +27,50 @@ angular.module('bookloopApp')
       'Karma'
     ];
 
-// Find books by title, author, or ISBN
-// Get an xml response with the most popular books for the given query. This will search all books in the title/author/ISBN fields and show matches, sorted by popularity on Goodreads. There will be cases where a result is shown on the Goodreads site, but not through the API. This happens when the result is an Amazon-only edition and we have to honor Amazon's terms of service. 
-// URL: https://www.goodreads.com/search/index.xml    (sample url) 
-// HTTP method: GET 
-// Parameters: 
-// q: The query text to match against book title, author, and ISBN fields. Supports boolean operators and phrase searching.
-// page: Which page to return (default 1, optional)
-// key: Developer key (required).
-// search[field]: Field to search, one of 'title', 'author', or 'genre' (default is 'all')
+ $scope.login = function(user){
+ 	console.log(user);
+ 	console.log(user.email);
+ 	console.log(user.password);
+ 	$scope.cfdump = '';
+    var request = $http({
+              method: "POST",
+              url: "https://localhost:3000/users/login",
+              transformRequest: transformRequestAsFormPost,
+              data: {
+                  email: user.email,
+                  password: user.password
+              }
+          });
+          // Store the data-dump of the FORM scope.
+          request.success(
+              function( html ) {
 
-// key: rY2wByZTY2NrSoem61slA
-// secret: pw9DEHT0JVej9mi6GniLTdjuiP2mimpbusPDeVHnU
+                  $scope.cfdump = html;
+
+              }
+          );
+ };  
+
+
+
+
+
 
 // GET https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=newest&key=yourAPIKey
 
-    // AIzaSyAUGRj4KWtNKx9Jg50n-5wKzYG_CiUTAPs
+// AIzaSyAUGRj4KWtNKx9Jg50n-5wKzYG_CiUTAPs
 
-  	$scope.search = function(book){
-  		$http.get('https://www.googleapis.com/books/v1/volumes?q='+book+'&key=AIzaSyAUGRj4KWtNKx9Jg50n-5wKzYG_CiUTAPs').
-  		success(function(data, status, headers, config){
-  			console.log(data);
-  		}).
-  		error(function(data, status, headers, config){
-  			console.log(status)
-  		})
-  	};
+  	// $scope.search = function(book){
+  	// 	$http.get('https://www.googleapis.com/books/v1/volumes?q='+book+'&key=AIzaSyAUGRj4KWtNKx9Jg50n-5wKzYG_CiUTAPs').
+  	// 	success(function(data, status, headers, config){
+  	// 		// console.log(data.items);
+  	// 		for (var i = data.items.length - 1; i >= 0; i--) {
+  	// 			console.log(data.items[i].volumeInfo)
+  	// 		};
+  	// 	}).
+  	// 	error(function(data, status, headers, config){
+  	// 		console.log(status);
+  	// 	})
+  	// };
 
  }]);
